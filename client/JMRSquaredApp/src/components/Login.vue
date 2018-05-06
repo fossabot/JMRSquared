bb87fbdd1e22b2428785e4ddba308d252c3579c6
Sirwali Joseph <mulavhe@gmail.com>
2018-05-06 12:27:36 +0200

    Refactored the Login Page.

--- client/JMRSquaredApp/src/components/Login.vue
+++ client/JMRSquaredApp/src/components/Login.vue
@@ -1,30 +1,24 @@
 <template>
   <page @loaded="pageLoaded()">
     <ActionBar>
-      <GridLayout rows="auto" columns="auto,*,auto" orientation="horizontal">
+      <GridLayout rows="auto" columns="auto,*,auto,auto" orientation="horizontal">
         <Ripple class="p-x-15" @tap="$router.back()" verticalAlignment="center" col="0" height="100%" borderRadius="50%">
           <Label verticalAlignment="center" class="mdi" fontSize="25%" :text="'mdi-arrow-back' | fonticon"></Label>
         </Ripple>
         <Label col="1" class="m-l-25 font-weight-bold" verticalAlignment="center" text="Login"></Label>
-        <Ripple class="p-x-15" @tap="reportBug()" verticalAlignment="center" col="2" height="100%" borderRadius="50%">
+        <Ripple class="p-x-15 font-weight-bold" @tap="isEnterEmail = !isEnterEmail" verticalAlignment="center" col="2">
+          <Label verticalAlignment="center" :text="isEnterEmail ? 'Use numbers' : 'Use email'"></Label>
+        </Ripple>
+        <Ripple class="p-x-15" @tap="reportBug()" verticalAlignment="center" col="3" height="100%" borderRadius="50%">
           <Label verticalAlignment="center" class="mdi" fontSize="25%" :text="'mdi-bug-report' | fonticon"></Label>
         </Ripple>
       </GridLayout>
     </ActionBar>
     <ScrollView width="100%">
-      <CardView verticalAlignment="center" margin="20" elevation="40" shadowOffsetHeight="10" shadowOpacity="0.2" shadowRadius="50">
+      <CardView verticalAlignment="center" padding="10" margin="4" elevation="10" shadowOffsetHeight="10" shadowOpacity="0.2" shadowRadius="50">
         <GridLayout width="100%">
           <FlexboxLayout class="m-10" justifyContent="space-between" width="100%" alignSelf="center" height="100%" flexDirection="column">
-            <CardView padding="10" margin="4" elevation="10" shadowOffsetHeight="10" shadowOpacity="0.2" shadowRadius="50">
-              <Image alignSelf="flex-start" src="~/images/logo/JMR-blue.png" width="40" height="40" />
-              <Label textAlignment="center" class="h1" text="Login"></Label>
-            </CardView>
-  
-            <FlexboxLayout @tap="isEnterEmail = !isEnterEmail" justifyContent="flex-end" alignSelf="flex-end" flexDirection="" orientation="horizontal" class="m-10" width="100%" textAlignment="right">
-              <Label class="h3 m-r-10 font-weight-bold" verticalAlignment="center" alignSelf="right" textAlignment="right" text="Use email instead"></Label>
-              <Switch verticalAlignment="center" alignSelf="right" textAlignment="right" v-model="isEnterEmail" checked="true" />
-            </FlexboxLayout>
-  
+           
             <GridLayout v-show="!isEnterEmail" class="m-10" rows="auto,auto" columns="auto,*">
               <label row="0" rowSpan="2" col="0" verticalAlignment="center" textAlignment="center" class="mdi m-10" fontSize="25%" :text="'mdi-phone' | fonticon"></label>
               <label row="0" col="1" class="h3 font-weight-bold text-mute" text="Contact numbers"></label>
@@ -36,14 +30,12 @@
               <label row="0" col="1" class="h3 font-weight-bold text-mute" text="Email"></label>
               <TextField row="1" col="1" :isEnabled="isEnterEmail" keyboardType="email" returnKeyType="next" v-model="user.email" autocorrect="true" autocapitalizationType="none" :class="{ 'light hidden': isEnterEmail}" :hidden="!isEnterEmail"></TextField>
             </GridLayout>
-            <StackLayout width="100%" class="hr-light"></StackLayout>
   
             <GridLayout class="m-10" rows="auto,auto" columns="auto,*">
               <label row="0" rowSpan="2" col="0" verticalAlignment="center" textAlignment="center" class="mdi m-10" fontSize="25%" :text="'mdi-lock' | fonticon"></label>
               <label row="0" col="1" class="h3 font-weight-bold text-mute" text="Password"></label>
               <TextField row="1" col="1" ref="password" secure="true" returnKeyType="done" v-model="user.password" @returnPress="submit()" :class="{ light: !isLoading }"></TextField>
             </GridLayout>
-            <StackLayout width="100%" class="hr-light"></StackLayout>
   
             <ActivityIndicator :busy="isLoading"></ActivityIndicator>
   
