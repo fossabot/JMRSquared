96d558147358e73004e58d7a5785f9e095f582a4
Sirwali Joseph <mulavhe@gmail.com>
2018-05-20 11:48:34 +0200

    Fixed the _id problem.

--- server/routes/AdminController.js
+++ server/routes/AdminController.js
@@ -1,14 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 // import the models
-import Admin from '../models/Admin';
-import Document from '../models/Document';
-import Transaction from '../models/Transaction';
-import Bug from '../models/Bug';
-import Rent from '../models/Rent';
-import Student from '../models/Student';
-
+import Admin from "../models/Admin";
+import Document from "../models/Document";
+import Transaction from "../models/Transaction";
+import Bug from "../models/Bug";
+import Rent from "../models/Rent";
+import Student from "../models/Student";
 
 /*
   TODO : Add admin
@@ -19,242 +18,272 @@ import Student from '../models/Student';
                 - 
 */
 
-router.get('/all', function(req, res) {
-    Admin.find().populate(['documents']).populate(['notifications']).then((admins) => {
-        if (admins == null) res.send("Error : 9032rtu834g9erbo");
-        res.json(admins);
+router.get("/all", function(req, res) {
+  Admin.find()
+    .populate(["documents"])
+    .populate(["notifications"])
+    .then(admins => {
+      if (admins == null) res.send("Error : 9032rtu834g9erbo");
+      res.json(admins);
     });
 });
 
-router.get('/GetById/:adminID', function(req, res) {
-    var adminID = req.params.adminID;
-    Admin.findById(adminID).populate(['documents']).populate(['notifications']).then((admin) => {
-        if (admin == null) {
-            res.status(500);
-            res.send("Invalid user");
-        } else {
-            res.json(admin);
-        }
-    }).catch(err => {
+router.get("/GetById/:adminID", function(req, res) {
+  var adminID = req.params.adminID;
+  Admin.findById(adminID)
+    .populate(["documents"])
+    .populate(["notifications"])
+    .then(admin => {
+      if (admin == null) {
         res.status(500);
-        res.send("Unable to log you in");
+        res.send("Invalid user");
+      } else {
+        res.json(admin);
+      }
+    })
+    .catch(err => {
+      res.status(500);
+      res.send("Unable to log you in");
     });
 });
 
+router.post("/login", function(req, res) {
+  if (req.body.useEmail) {
+    Admin.findOne({
+      email: req.body.email,
+      pass: req.body.pass
+    })
+      .populate(["documents"])
+      .populate(["notifications"])
+      .then(admin => {
+        if (admin == null) {
+          res.status(500);
+          res.send("Incorrect log in details");
+        } else {
+          res.json(admin);
+        }
+      });
+  } else {
+    Admin.findOne({
+      numbers: req.body.numbers,
+      pass: req.body.pass
+    })
+      .populate(["documents"])
+      .populate(["notifications"])
+      .then(admin => {
+        if (admin == null) {
+          res.status(500);
+          res.send("Incorrect log in details");
+        } else {
+          res.json(admin);
+        }
+      });
+  }
+});
 
-router.post('/login', function(req, res) {
-    if(req.body.useEmail){
-        Admin.findOne({
-            email: req.body.email,
-            pass: req.body.pass
-        }).populate(['documents']).populate(['notifications']).then((admin) => {
-            if (admin == null) {
-                res.status(500);
-                res.send("Incorrect log in details");
-            } else {
-                res.json(admin);
-            }
-        })
-    }else{
-        Admin.findOne({
-            numbers: req.body.numbers,
-            pass: req.body.pass
-        }).populate(['documents']).populate(['notifications']).then((admin) => {
-            if (admin == null) {
-                res.status(500);
-                res.send("Incorrect log in details");
-            } else {
-                res.json(admin);
-            }
-        })
+router.post("/add", function(req, res) {
+  var admin = new Admin({
+    _id: mongoose.Types.ObjectId(),
+    email: req.body.email,
+    pass: req.body.pass,
+    numbers: req.body.numbers,
+    userName: req.body.username
+  });
+
+  admin.save(function(err) {
+    if (err) res.send(err);
+    res.send("User successfully saved");
+  });
+});
+
+router.get("/bug/get/:bugId", function(req, res) {
+  var bugID = req.params.bugId;
+  Bug.findById(bugID).then(bug => {
+    if (bug == null) {
+      res.status(500);
+      res.send("Invalid request");
+    } else {
+      res.json(bug);
     }
+  });
 });
 
-router.post('/add', function(req, res) {
+router.get("/bug/all", function(req, res) {
+  Bug.find({}, "_id senderName senderPic bugText date").then(bugs => {
+    if (bugs == null) {
+      res.status(500);
+      res.send("Error : 9032rtu834g9erbo");
+    }
+    bugs.reverse();
+    res.json(bugs);
+  });
+});
 
-    var admin = new Admin({
-        email: req.body.email,
-        pass: req.body.pass,
-        numbers:req.body.numbers,
-        userName:req.body.username
+router.post("/bug/add", function(req, res) {
+  var bug = new Bug({
+    _id: mongoose.Types.ObjectId(),
+    senderName: req.body.senderName,
+    senderPic: req.body.senderPic,
+    bugText: req.body.bugText
+  });
+
+  bug.save(function(err) {
+    if (err) {
+      res.status(500);
+      res.send(err);
+    }
+    res.send("Bug added successfully saved");
+  });
+});
+
+router.get("/document/get/:documentId", function(req, res) {
+  var documentID = req.params.documentId;
+  Document.findById(documentID).then(document => {
+    if (document == null) {
+      res.status(500);
+      res.send("Invalid request");
+    } else {
+      res.json(document);
+    }
+  });
+});
+
+router.get("/document/all", function(req, res) {
+  Document.find({}, "_id title description type date").then(documents => {
+    if (documents == null) {
+      res.status(500);
+      res.send("Error : 9032rtu834g9erbo");
+    }
+    documents.reverse();
+    res.json(documents);
+  });
+});
+
+router.post("/document/add", function(req, res) {
+  var document = new Document({
+    _id: mongoose.Types.ObjectId(),
+    title: req.body.title,
+    adminID: req.body.adminID,
+    location: req.body.location,
+    thumbnail: req.body.thumbnail,
+    description: req.body.description,
+    type: req.body.type
+  });
+
+  document.save(function(err) {
+    if (err) {
+      res.status(500);
+      res.send(err);
+    }
+    res.send("Document added successfully saved");
+  });
+});
+
+router.get("/transaction/get/:transactionId", function(req, res) {
+  var transactionID = req.params.transactionId;
+  Transaction.findById(transactionID)
+    .populate("adminID", "userName")
+    .then(transaction => {
+      if (transaction == null) {
+        res.status(500);
+        res.send("Invalid request");
+      } else {
+        res.json(transaction);
+      }
     });
-
-    admin.save(function(err) {
-        if (err) res.send(err);
-        res.send("User successfully saved");
-    })
-
 });
 
-router.get('/bug/get/:bugId',function(req,res){
-    var bugID = req.params.bugId;
-    Bug.findById(bugID).then((bug) => {
-        if (bug == null) {
-            res.status(500);
-            res.send("Invalid request");
-        } else {
-            res.json(bug);
-        }
-    })
-});
-
-router.get('/bug/all', function(req, res) {
-    Bug.find({},'_id senderName senderPic bugText date').then((bugs) => {
-        if (bugs == null){
-            res.status(500);
-            res.send("Error : 9032rtu834g9erbo");
-        } 
-        bugs.reverse();
-        res.json(bugs);
+router.get("/transaction/all", function(req, res) {
+  Transaction.find(
+    {},
+    "_id adminID amount type rentTenantName rentMonth description date"
+  )
+    .populate("adminID", "userName")
+    .then(transactions => {
+      if (transactions == null) {
+        res.status(500);
+        res.send("Error : 9032rtu834g9erbo");
+      }
+      transactions.reverse();
+      res.json(transactions);
     });
 });
 
-router.post('/bug/add', function(req, res) {
-    var bug = new Bug({
-        senderName: req.body.senderName,
-        senderPic: req.body.senderPic,
-        bugText:req.body.bugText
-    });
+router.post("/transaction/add", function(req, res) {
+  var transaction = new Transaction({
+    _id: mongoose.Types.ObjectId(),
+    adminID: req.body.adminID, //ForeignKey
+    amount: req.body.amount,
+    type: req.body.type,
+    rentTenantID: req.body.rentTenantID,
+    rentTenantName: req.body.rentTenantName,
+    rentMonth: req.body.rentMonth,
+    description: req.body.description,
+    proof: req.body.proof,
+    date: req.body.date
+  });
 
-    bug.save(function(err) {
+  transaction.save(function(err) {
+    if (err) res.send(err);
+    Admin.findById(req.body.adminID, function(err, admin) {
+      if (err || admin == null) {
+        console.log(err);
+        return;
+      }
+
+      admin.transactions.push(transaction._id);
+      admin.save(function(err) {
         if (err) {
-            res.status(500);
-            res.send(err);
+          console.log(err);
+          return;
         }
-        res.send("Bug added successfully saved");
-    })
+        if (req.body.type == "Rent") {
+          var rent = new Rent({
+            _id: mongoose.Types.ObjectId(),
+            studentID: req.body.rentTenantID,
+            datePaid: req.body.date,
+            monthOfPayment: req.body.rentMonth,
+            amount: req.body.amount
+          });
 
-});
-
-
-router.get('/document/get/:documentId',function(req,res){
-    var documentID = req.params.documentId;
-    Document.findById(documentID).then((document) => {
-        if (document == null) {
-            res.status(500);
-            res.send("Invalid request");
-        } else {
-            res.json(document);
-        }
-    })
-});
-
-router.get('/document/all', function(req, res) {
-    Document.find({},'_id title description type date').then((documents) => {
-        if (documents == null){
-            res.status(500);
-            res.send("Error : 9032rtu834g9erbo");
-        } 
-        documents.reverse();
-        res.json(documents);
-    });
-});
-
-router.post('/document/add', function(req, res) {
-    var document = new Document({
-        title: req.body.title,
-        adminID:req.body.adminID,
-        location: req.body.location,
-        thumbnail:req.body.thumbnail,
-        description:req.body.description,
-        type:req.body.type
-    });
-
-    document.save(function(err) {
-        if (err) {
-            res.status(500);
-            res.send(err);
-        }
-        res.send("Document added successfully saved");
-    })
-
-});
-
-router.get('/transaction/get/:transactionId',function(req,res){
-    var transactionID = req.params.transactionId;
-    Transaction.findById(transactionID).populate('adminID','userName').then((transaction) => {
-        if (transaction == null) {
-            res.status(500);
-            res.send("Invalid request");
-        } else {
-            res.json(transaction);
-        }
-    })
-});
-
-router.get('/transaction/all', function(req, res) {
-    Transaction.find({},'_id adminID amount type rentTenantName rentMonth description date').populate('adminID','userName').then((transactions) => {
-        if (transactions == null){
-            res.status(500);
-            res.send("Error : 9032rtu834g9erbo");
-        } 
-        transactions.reverse();
-        res.json(transactions);
-    });
-});
-
-router.post('/transaction/add', function(req, res) {
-
-    var transaction = new Transaction({
-        adminID:req.body.adminID, //ForeignKey
-        amount: req.body.amount,
-        type: req.body.type,
-        rentTenantID:req.body.rentTenantID,
-        rentTenantName: req.body.rentTenantName,
-        rentMonth: req.body.rentMonth,
-        description: req.body.description,
-        proof: req.body.proof,
-        date:req.body.date
-    });
-
-    transaction.save(function(err) {
-        if (err) res.send(err);
-        Admin.findById(req.body.adminID,function(err,admin){
-            if (err || admin == null)  { console.log(err); return; };
-
-            admin.transactions.push(transaction._id);
-            admin.save(function(err){
-                if (err)  { console.log(err); return; };
-                if(req.body.type == 'Rent'){
-                    var rent = new Rent({
-                        studentID :req.body.rentTenantID,
-                        datePaid :req.body.date,
-                        monthOfPayment :req.body.rentMonth,
-                        amount :req.body.amount
-                    });
-                   
-                    rent.save(function(err){
-                        if (err)  { console.log(err); return; };
-                        Student.findById(req.body.rentTenantID,function(err,student){
-                            if (err || student == null)  { console.log(err); return; };
-                            student.rents.push(rent._id);
-                            student.save(function(err){
-                                if (err)  { console.log(err); return; };
-                                res.send("Rent payment successfully saved");
-                            });
-                        });
-                    });
-                }else{
-
-                    console.log("On else");
-                    res.send("Transaction successfully saved");
+          rent.save(function(err) {
+            if (err) {
+              console.log(err);
+              return;
+            }
+            Student.findById(req.body.rentTenantID, function(err, student) {
+              if (err || student == null) {
+                console.log(err);
+                return;
+              }
+              student.rents.push(rent._id);
+              student.save(function(err) {
+                if (err) {
+                  console.log(err);
+                  return;
                 }
+                res.send("Rent payment successfully saved");
+              });
             });
-        });
-    })
-
+          });
+        } else {
+          console.log("On else");
+          res.send("Transaction successfully saved");
+        }
+      });
+    });
+  });
 });
 
+router.get("/notifications/all", function(req, res) {
+  Notification.find({ dueDate: null }).then(result => {
+    if (result == null) {
+      res.status(400);
+      res.send("Error : 9032egrrtu834g9erbo");
+    }
 
-router.get('/notifications/all', function(req, res) {
-    Notification.find({dueDate:null}).then((result) => {
-        if (result == null) {
-            res.status(400);
-            res.send("Error : 9032egrrtu834g9erbo");
-        }
-
-        res.json(result);
-    });
+    res.json(result);
+  });
 });
 
 module.exports = router;
--- server/routes/StudentController.js
+++ server/routes/StudentController.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-import Student from '../models/Student';
+import Student from "../models/Student";
 
-import Rent from '../models/Rent';
+import Rent from "../models/Rent";
 
 /*
   TODO: Get one student - DONE
@@ -15,153 +15,159 @@ import Rent from '../models/Rent';
         Login student
 */
 
-router.get('/rents/all', function(req, res) {
-    Rent.find().then((rents) => {
-        if (rents == null) res.send("Error : 90ty32rtu834g9erbo");
-        res.json(rents);
+router.get("/rents/all", function(req, res) {
+  Rent.find().then(rents => {
+    if (rents == null) res.send("Error : 90ty32rtu834g9erbo");
+    res.json(rents);
+  });
+});
+
+router.get("/students/all", function(req, res) {
+  Student.find()
+    .populate(["rents"])
+    .then(students => {
+      if (students == null) res.send("Error : 9032rtu834g9erbo");
+      res.json(students);
     });
 });
 
-router.get('/students/all', function(req, res) {
-    Student.find().populate(['rents']).then((students) => {
-        if (students == null) res.send("Error : 9032rtu834g9erbo");
-        res.json(students);
-    });
+router.get("/students/all/names", function(req, res) {
+  Student.find({}, "_id username").then(students => {
+    if (students == null) res.send("Error : 9032rtu834g9erbo");
+    res.json(students);
+  });
 });
 
-router.get('/students/all/names', function(req, res) {
-    Student.find({},'_id username').then((students) => {
-        if (students == null) res.send("Error : 9032rtu834g9erbo");
-        res.json(students);
-    });
-});
-
-router.get('/:id/get', function(req, res) {
-    let id = req.params.id;
-    if (id == null) {
+router.get("/:id/get", function(req, res) {
+  let id = req.params.id;
+  if (id == null) {
+    res.status(404);
+    res.send("Invalid ID > " + id);
+  } else {
+    Student.findById(id).then(student => {
+      if (student == null) {
         res.status(404);
-        res.send("Invalid ID > " + id);
-    } else {
-        Student.findById(id).then((student) => {
-            if (student == null) {
-                res.status(404);
-                res.send("No student with id : " + id);
-            } else {
-                res.json(student);
-            }
-        });
-    }
+        res.send("No student with id : " + id);
+      } else {
+        res.json(student);
+      }
+    });
+  }
 });
-
 
 /**
  * POST methods
  */
 
-router.post('/:text/search', function(req, res) {
-    let text = req.params.text;
-    if (text == null || text.length < 3) {
-        res.status(404);
-        res.send("Cannot search for - " + text);
-    } else {
-        Student.find({
-            where: {
-                [Op.or]: [{
-                    lastName: {
-                        [Op.like]: '%' + text + '%'
-                    }
-                }, {
-                    firstName: {
-                        [Op.like]: '%' + text + '%'
-                    }
-                }]
+router.post("/:text/search", function(req, res) {
+  let text = req.params.text;
+  if (text == null || text.length < 3) {
+    res.status(404);
+    res.send("Cannot search for - " + text);
+  } else {
+    Student.find({
+      where: {
+        [Op.or]: [
+          {
+            lastName: {
+              [Op.like]: "%" + text + "%"
             }
-        }).then((answer) => {
-            if (answer == null || answer.length != 1) {
-                res.send("firstname or lastname that match : " + text);
-            } else {
-                res.json(answer);
+          },
+          {
+            firstName: {
+              [Op.like]: "%" + text + "%"
             }
-        });
-    }
-});
-
-router.post('/login', function(req, res) {
-    //Dont forget this is just to disable the nigga
-    if(req.body.useEmail){
-        Student.findOne({
-            email: req.body.email,
-            password: req.body.pass
-        }).then((student) => {
-            if (student == null) {
-                res.status(500)
-                res.send("Incorrect log in details");
-            } else {
-                res.json(student);
-            }
-        })
-    }else{
-        Student.findOne({
-            contactNumbers: req.body.numbers,
-            password: req.body.pass
-        }).then((student) => {
-            if (student == null) {
-                res.status(500)
-                res.send("Incorrect log in details");
-            } else {
-                res.json(student);
-            }
-        })
-    }
-    
-});
-
-router.post('/add', function(req, res) {
-    console.log(req.body);
-    Student.find().or([{username:req.body.username },{contactNumbers:req.body.contactNumbers}, { firstName: req.body.firstName ,surname:req.body.surname  }]).then((s) =>{
-        console.log(s);
-        if(s.length == 0){
-
-            if(req.body.username == null || req.body.username.length < 2){
-                res.status(404);
-                res.send("Username is required");
-            } else if (req.body.firstName == null || req.body.surname == null) {
-                res.status(404);
-                res.send("Name can not be null");
-            } else if (req.body.contactNumbers.length != 10 && !isNaN(req.body.contactNumbers)) {
-                res.status(404);
-                res.send("Contact numbers must be 10 digits long");
-            } else {
-                var student = new Student(req.body);
-            student.save(function(err) {
-                    if (err){
-                        console.log("Error .....");
-                        console.log(err);
-                        res.status(402);
-                        res.send(err);
-                    
-                    }
-                    res.send(student._id);
-                });
-            }
-
-        }else {
-            res.status(400);
-            res.send("User already exist");
-        }
-
+          }
+        ]
+      }
+    }).then(answer => {
+      if (answer == null || answer.length != 1) {
+        res.send("firstname or lastname that match : " + text);
+      } else {
+        res.json(answer);
+      }
     });
-
+  }
 });
 
-router.post('/:id/remove', function(req, res) {
-    //Dont forget this is just to disable the nigga
-
+router.post("/login", function(req, res) {
+  //Dont forget this is just to disable the nigga
+  if (req.body.useEmail) {
+    Student.findOne({
+      email: req.body.email,
+      password: req.body.pass
+    }).then(student => {
+      if (student == null) {
+        res.status(500);
+        res.send("Incorrect log in details");
+      } else {
+        res.json(student);
+      }
+    });
+  } else {
+    Student.findOne({
+      contactNumbers: req.body.numbers,
+      password: req.body.pass
+    }).then(student => {
+      if (student == null) {
+        res.status(500);
+        res.send("Incorrect log in details");
+      } else {
+        res.json(student);
+      }
+    });
+  }
 });
 
-router.post('/:id/update', function(req, res) {
-    //Dont forget this is just to disable the nigga
+router.post("/add", function(req, res) {
+  console.log(req.body);
+  req.body._id = mongoose.Types.ObjectId();
+  Student.find()
+    .or([
+      { username: req.body.username },
+      { contactNumbers: req.body.contactNumbers },
+      { firstName: req.body.firstName, surname: req.body.surname }
+    ])
+    .then(s => {
+      console.log(s);
+      if (s.length == 0) {
+        if (req.body.username == null || req.body.username.length < 2) {
+          res.status(404);
+          res.send("Username is required");
+        } else if (req.body.firstName == null || req.body.surname == null) {
+          res.status(404);
+          res.send("Name can not be null");
+        } else if (
+          req.body.contactNumbers.length != 10 &&
+          !isNaN(req.body.contactNumbers)
+        ) {
+          res.status(404);
+          res.send("Contact numbers must be 10 digits long");
+        } else {
+          var student = new Student(req.body);
+          student.save(function(err) {
+            if (err) {
+              console.log("Error .....");
+              console.log(err);
+              res.status(402);
+              res.send(err);
+            }
+            res.send(student._id);
+          });
+        }
+      } else {
+        res.status(400);
+        res.send("User already exist");
+      }
+    });
+});
 
+router.post("/:id/remove", function(req, res) {
+  //Dont forget this is just to disable the nigga
+});
+
+router.post("/:id/update", function(req, res) {
+  //Dont forget this is just to disable the nigga
 });
 
 module.exports = router;
