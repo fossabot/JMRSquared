1a7ead41d0d2a99795adfdaa045cac2689ad8e99
Sirwali Joseph <mulavhe@gmail.com>
2018-05-20 10:54:55 +0200

    Fix the develop to use the test DB and port 3000

--- server/app.js
+++ server/app.js
@@ -1,31 +1,34 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require("express");
+var path = require("path");
+var favicon = require("static-favicon");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
 
 var app = express();
 
-import mongoose from 'mongoose';
-import adminController from './routes/AdminController';
-import studentController from './routes/StudentController';
-import notificationController from './routes/NotificationController';
+import mongoose from "mongoose";
+import adminController from "./routes/AdminController";
+import studentController from "./routes/StudentController";
+import notificationController from "./routes/NotificationController";
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
 app.use(favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb'}));
+app.use(logger("dev"));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb" }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 // Local Host
 /*
@@ -40,54 +43,53 @@ mongoose.connect("mongodb://localhost:27017/JMRdb", {
 
 //    Server
 
-mongoose.connect("mongodb://localhost:27017/JMRdb?authSource=admin", {
+mongoose
+  .connect("mongodb://localhost:27017/JMRdb?authSource=admin", {
     auth: {
-        user: "admin",
-        password: "Mulavhelesi@1"
+      user: "admin",
+      password: "Mulavhelesi@1"
     },
     reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
     reconnectInterval: 500, // Reconnect every 500ms
-    dbName: 'JMRdb'
-}).then((answer) => {
+    dbName: "JMRdb"
+  })
+  .then(answer => {
     console.log("Successfully connected to MONGO!");
-});
+  });
 
-
-app.use('/s', studentController);
-app.use('/a', adminController);
-app.use('/n', notificationController);
+app.use("/s", studentController);
+app.use("/a", adminController);
+app.use("/n", notificationController);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
 });
-
 
 /// error handlers
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+if (app.get("env") === "development") {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render("error", {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render("error", {
+    message: err.message,
+    error: {}
+  });
 });
-
 
 module.exports = app;
