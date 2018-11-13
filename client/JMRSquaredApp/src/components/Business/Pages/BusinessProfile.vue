d1dc31ba867dd20ad23cfe731b0a31b6de6a9600
Lavhe <jsirwali@retrorabbit.co.za>
2018-11-13 08:15:57 +0200

    Added PartnersList and its pop up on BusinessProfile

--- client/JMRSquaredApp/src/components/Business/Pages/BusinessProfile.vue
+++ client/JMRSquaredApp/src/components/Business/Pages/BusinessProfile.vue
@@ -1,23 +1,59 @@
 <template>
-  <GridLayout class="m-x-15" rows="auto,*">
-    <StackLayout row="0">
-      <Image src="res://jmrlogo" alignSelf="center" stretch="aspectFit" />
-    </StackLayout>
+  <GridLayout class="p-x-15" columns="*" rows="auto,auto,*">
+    <Image row="0" width="100" height="100" src="res://jmrlogo" alignSelf="center" stretch="aspectFit" />
     <StackLayout row="1">
+      <label textAlignment="center" verticalAlignment="center" fontSize="25%" class="h3 font-weight-bold text-light-red" :text="business.name"></label>
+    </StackLayout>
+    <StackLayout row="2">
       <ScrollView>
         <StackLayout>
           <GridLayout rows="auto" columns="*">
             <label row="0" col="0" class="h3 font-weight-bold text-mute text-dark-blue" text="Summary"></label>
           </GridLayout>
-  
           <CardView margin="15" elevation="10">
-            <GridLayout rows="auto,auto" columns="*,*,*">
-              <StackLayout class="p-15" :col="i" v-for="(summary,i) in summaryStats" :key="i">
-                <label row="0" class="h3 font-weight-bold text-dark-blue summaryStats" :text="summary.value" :class="{'visible':true}" fontSize="30%" vertialAlignment="center" textAlignment="center"></label>
-                <label row="1" vertialAlignment="center" textAlignment="center" :text="summary.title"></label>
+            <GridLayout rows="*,*" columns="*,*,*">
+              <StackLayout row="0" :col="i" v-for="(summary,i) in summaryStats" :key="i">
+                <Ripple @tap="showPartners()">
+                  <StackLayout class="p-15">
+                    <label class="h3 font-weight-bold text-dark-blue summaryStats" :text="summary.value" :class="{'visible':true}" fontSize="30%" vertialAlignment="center" textAlignment="center"></label>
+                    <label vertialAlignment="center" textAlignment="center" :text="summary.title"></label>
+                  </StackLayout>
+                </Ripple>
               </StackLayout>
             </GridLayout>
           </CardView>
+  
+          <GridLayout rows="auto" columns="*">
+            <label row="0" col="0" class="h3 font-weight-bold text-mute text-dark-blue" text="Stats"></label>
+          </GridLayout>
+  
+          <GridLayout rows="auto" columns="*,*">
+            <CardView class="p-15" row="0" col="0" margin="5" elevation="1">
+              <GridLayout rows="auto,auto" columns="*,*,*">
+                <label row="0" col="0" colSpan="2" verticalAlignment="center" class="h3 font-weight-bold text-mute text-light-blue p-5" text="Expenses"></label>
+                <Ripple row="0" col="2" class="p-5" textAlignment="right" verticalAlignment="center" borderRadius="50%">
+                  <Label verticalAlignment="center" class="mdi" fontSize="25%" :text="'mdi-add' | fonticon"></Label>
+                </Ripple>
+                <StackLayout row="1" col="0" class="p-15 b-r-15">
+                  <label row="0" class="h3 font-weight-bold text-dark-blue" fontSize="30%" text="1"></label>
+                  <label row="1" vertialAlignment="center" textAlignment="center" text="3"></label>
+                </StackLayout>
+                <StackLayout row="1" col="1" class="p-15">
+                  <label row="0" class="h3 font-weight-bold text-dark-blue" fontSize="30%" text="1"></label>
+                  <label row="1" vertialAlignment="center" textAlignment="center" text="3"></label>
+                </StackLayout>
+              </GridLayout>
+            </CardView>
+            <CardView class="p-15" row="0" col="1" margin="5" elevation="1">
+              <GridLayout rows="auto,auto" columns="*,*,*">
+                <label row="0" col="0" colSpan="2" class="h3 font-weight-bold text-mute text-light-blue p-5" text="Activities"></label>
+                <StackLayout row="1" col="0" class="p-15">
+                  <label row="0" class="h3 font-weight-bold text-dark-blue" fontSize="30%" text="1"></label>
+                  <label row="1" vertialAlignment="center" textAlignment="center" text="3"></label>
+                </StackLayout>
+              </GridLayout>
+            </CardView>
+          </GridLayout>
         </StackLayout>
       </ScrollView>
     </StackLayout>
@@ -28,7 +64,12 @@
 const dialogs = require("ui/dialogs");
 import application from "application";
 
+import PartnersList from '../../Modals/PartnersList'
+
 export default {
+  components:{
+    PartnersList
+  },
   data() {
     return {
       summaryStats: [],
@@ -75,7 +116,13 @@ export default {
     });
   },
   props: ["business"],
-  methods: {}
+  methods: {
+    showPartners() {
+    this.$showModal(PartnersList, { 
+      props: { businessName: "this.business.name" }
+    });
+    }
+  }
 };
 </script>
 
@@ -104,5 +151,9 @@ export default {
       opacity: 1;
     }
   }
+}
+
+.b-r-15 {
+  border: 2px solid red;
 }
 </style>
---
+++ client/JMRSquaredApp/src/components/Modals/PartnersList.vue
@@ -0,0 +1,39 @@
+<template>
+  <StackLayout backgroundColor="white">
+    <Label row="0" class="h2 m-15" textAlignment="center" :text="`All partners of ${businessName}`"></Label>
+    <ScrollView>
+              <StackLayout>                     
+          <Ripple class="m-x-15" v-for="a in 20" :key="a">
+          <GridLayout padding="2%" columns="auto,*,auto" rows="auto,auto,auto">
+            <Image row="0" rowSpan="3" col="0" borderWidth="5px" borderColor="$blueLightColor" stretch="aspectFill" :src="$store.state.settings.defaultProfilePic" width="70" height="70"
+                borderRadius="50%" />
+            <Label row="0" col="1" textAlignment="center" class="h2" text="student.username"></Label>
+            <Label row="1" col="1" textAlignment="center" text="student.email"></Label>
+            <Label row="2" col="1" textAlignment="center" text="'Room : ' + student.room"></Label>
+            <Label row="2" col="2" textAlignment="center" verticalAlignment="center" text="Paid"></Label>
+          </GridLayout>
+          </Ripple>
+          </StackLayout>
+      </ScrollView>
+    </StackLayout>
+</template>
+
+<script>
+
+export default {
+  name: "PartnersList",
+  data() {
+    return {};
+  },
+  props:[
+    'businessName'
+  ],
+  methods: {
+    
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/variables";
+</style>
