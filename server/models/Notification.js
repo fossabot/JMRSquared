e271b7e4b967f52445702b4acca0154cad380a15
Sirwali Joseph <mulavhe@gmail.com>
2018-12-10 22:33:19 +0200

    Get notifications with businessID and more info on the scheduled test

--- server/models/Notification.js
+++ server/models/Notification.js
@@ -1,60 +1,68 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 var Schema = mongoose.Schema;
 
 const NotificationSchema = new mongoose.Schema({
-    _id: {
-        type: Schema.Types.ObjectId,
-        default: mongoose.Types.ObjectId()
-    },
-    fromId: {
-        type: Schema.Types.ObjectId,
-        ref: 'Admin'
-    },
-    toId: {
-        type: Schema.Types.ObjectId,
-        ref: 'Admin'
-    },
-    topic: {
-        type: String,
-        default: null
-    },
-    title: String,
-    body: String,
-    data: Object,
-    type: {
-        type: String,
-        enum: ['REMINDER', 'INFO', 'WARNING', 'ERROR', 'SUCCESS'],
-        default: "REMINDER"
-    },
-    status: {
-        type: String,
-        enum: ['PENDING', 'SENT', 'SEEN'],
-        default: "PENDING"
-    },
-    date: {
-        type: Date,
-        default: Date.now()
-    },
-    scheduled: {
-        type: Boolean,
-        default: false
-    },
-    scheduleInterval: String, // cron format e.g. * * * * * (seconds{optional} minute hour day month year )
-    expiryDate: {
-        type: Date,
-        default: null
-    },
-    removed: {
-        type: Boolean,
-        default: false
-    }
+  _id: {
+    type: Schema.Types.ObjectId,
+    default: mongoose.Types.ObjectId()
+  },
+  fromId: {
+    type: Schema.Types.ObjectId,
+    ref: "Admin"
+  },
+  toId: {
+    type: Schema.Types.ObjectId,
+    ref: "Admin"
+  },
+  businessID: {
+    type: Schema.Types.ObjectId,
+    ref: "Business",
+    default: null
+  },
+  topic: {
+    type: String,
+    default: null
+  },
+  title: String,
+  body: String,
+  data: Object,
+  type: {
+    type: String,
+    enum: ["REMINDER", "INFO", "WARNING", "ERROR", "SUCCESS"],
+    default: "REMINDER"
+  },
+  status: {
+    type: String,
+    enum: ["PENDING", "SENT", "SEEN"],
+    default: "PENDING"
+  },
+  date: {
+    type: Date,
+    default: Date.now()
+  },
+  scheduled: {
+    type: Boolean,
+    default: false
+  },
+  scheduleInterval: String, // cron format e.g. * * * * * (seconds{optional} minute hour day month year )
+  expiryDate: {
+    type: Date,
+    default: null
+  },
+  removed: {
+    type: Boolean,
+    default: false
+  }
 });
 
 NotificationSchema.methods.findSimilarTypes = function(cb) {
-    return this.model('Animal').find({
-        type: this.type
-    }, cb);
+  return this.model("Animal").find(
+    {
+      type: this.type
+    },
+    cb
+  );
 };
 
-const Notification = mongoose.model('Notification', NotificationSchema);
+const Notification = mongoose.model("Notification", NotificationSchema);
 module.exports = Notification;
--- server/routes/NotificationController.js
+++ server/routes/NotificationController.js
@@ -20,263 +20,293 @@ const cronJob = new CronJob();
          Get all from user (done)
 */
 
-router.post("/test/push/notification", function (req, res) {
-    var deviceToken = req.body.token;
-    var payload = {
-        notification: {
-            title: "Testing push!!",
-            body: "This is the body of the push"
-        },
-        data: {
-            link: "/home",
-            props: JSON.stringify({
-                device_token: deviceToken
-            })
-        }
-    };
-    FCM.sendToDevice(deviceToken, payload)
-        .then(response => {
-            return res.json(response);
-        })
-        .catch(err => {
-            return res.status(512).send(err);
-        });
-});
-
-router.post("/push/notification/to/admin", function (req, res) {
-    var adminID = req.body.adminID;
-    var notification = req.body.notification;
-    var data = req.body.data;
-    var link = req.body.link;
-    var props = req.body.props;
-
-    if (!notification) {
-        notification = {
-            title: "Testing push!!",
-            body: "This is the body of the push"
-        }
+router.post("/test/push/notification", function(req, res) {
+  var deviceToken = req.body.token;
+  var payload = {
+    notification: {
+      title: "Testing push!!",
+      body: "This is the body of the push"
+    },
+    data: {
+      link: "/home",
+      props: JSON.stringify({
+        device_token: deviceToken
+      })
     }
-    Admin.findById(adminID).then(user => {
-        if (user == null) return res.status(514).send("User of id " + adminID + " not found");
-        var tokens = user.deviceTokens.filter(v => !v.removed).map(v => v.token);
-        if (tokens) {
-            var payload = {
-                notification: notification,
-                data: {
-                    link: link,
-                    props: JSON.stringify(props),
-                    data: JSON.stringify(data),
-
-                }
-            };
-            tokens.forEach(deviceToken => {
-                FCM.sendToDevice(deviceToken, payload)
-                    .then(response => {
-
-                    })
-                    .catch(err => {
-                        // return res.status(512).send(err);
-                    });
-            });
-            return res.json("Notification will be sent to " + tokens.length + " devices");
-        } else {
-            return res.status(514).send("User has no device");
-        }
-    }).catch(err => {
-        return res.status(514).send(err);
+  };
+  FCM.sendToDevice(deviceToken, payload)
+    .then(response => {
+      return res.json(response);
+    })
+    .catch(err => {
+      return res.status(512).send(err);
     });
 });
 
-router.post("/test/push/notification/schedule", function (req, res) {
-    var deviceToken = req.body.token;
-    var payload = {
-        notification: {
-            title: "Testing push!!",
-            body: "This is the body of the push"
-        },
-        data: {
-            link: "/home",
-            props: JSON.stringify({
-                device_token: deviceToken
-            })
-        }
+router.post("/push/notification/to/admin", function(req, res) {
+  var adminID = req.body.adminID;
+  var notification = req.body.notification;
+  var data = req.body.data;
+  var link = req.body.link;
+  var props = req.body.props;
+
+  if (!notification) {
+    notification = {
+      title: "Testing push!!",
+      body: "This is the body of the push"
     };
-    cronJob.schedule("30 5 * * *", deviceToken, payload);
-});
-
-router.get("/cron/get/tasks", function (req, res) {
-    res.send(cronJob.getTasks());
-});
-
-router.get("/get/new/for/:userId", function (req, res) {
-    var receiver = req.params.userId;
-    Notification.find({
-        $or: [{
-            expiryDate: null,
-        }, {
-            expiryDate: {
-                $lt: new Date()
-            }
-        }],
-        $or: [{
-            status: {
-                $ne: 'SEEN'
-            }
-        }],
-        removed: false,
-        toId: receiver
-    }).sort('-date').then(notifications => {
-        if (notifications == null) res.send("Error : 9032egrrtu834g9erbo");
-        res.json(notifications);
-    });
-});
-
-router.get("/all/for/:userId/:scheduled", function (req, res) {
-    var receiver = req.params.userId;
-    let scheduled = req.params.scheduled ? true : false;
-    Notification.find({
-        scheduled: scheduled,
-        $or: [{
-            expiryDate: null,
-        }, {
-            expiryDate: {
-                $lt: new Date()
-            }
-        }],
-        toId: receiver
-    }).then(notifications => {
-        if (notifications == null) res.send("Error : 9032egrrtu834g9erbo");
-        res.json(notifications);
-    });
-});
-
-router.get("/all/:userId/:scheduled", function (req, res) {
-    let sender = req.params.userId;
-    let scheduled = req.params.scheduled ? true : false;
-    Notification.find({
-        scheduled: scheduled,
-        $or: [{
-            expiryDate: null,
-        }, {
-            expiryDate: {
-                $lt: new Date()
-            }
-        }],
-        $or: [{
-            fromId: sender
-        }, {
-            toId: sender
-        }]
-    }).then(result => {
-        if (result == null) {
-            res.status(400);
-            res.send("Error : 9032egrrtu834g9erbo");
-        }
-
-        res.json(result);
-    });
-});
-
-router.get("/tasks/all", function (req, res) {
-    Notification.find({
-        dueDate: {
-            $ne: null
-        }
-    }).then(result => {
-        if (result == null) {
-            res.status(400);
-            res.send("Error : 9032egrrtu834g9erbo");
-        }
-        res.json(result);
-    });
-});
-
-router.get("/tasks/all/:userId", function (req, res) {
-    var sender = req.params.userId;
-    Notification.find({
-        dueDate: {
-            $ne: null
-        },
-        $or: [{
-            fromId: sender
-        }, {
-            toId: sender
-        }]
-    }).then(result => {
-        if (result == null) {
-            res.status(400);
-            res.send("Error : 9032egrrtu834g9erbo");
-        }
-        res.json(result);
-    });
-});
-
-router.post("/add", function (req, res) {
-    var notification = new Notification({
-        _id: mongoose.Types.ObjectId(),
-        fromId: req.body.fromId,
-        toId: req.body.toId,
-        title: req.body.title,
-        body: req.body.body,
-        data: req.body.data,
-        type: req.body.type,
-        status: req.body.status,
-        scheduled: req.body.scheduled,
-        topic: req.body.topic,
-        scheduleInterval: req.body.scheduleInterval,
-        expiryDate: req.body.expiryDate
-    });
-
-    notification.save(function (err) {
-        if (err) return res.status(512).send(err);
-
-        Admin.findById(req.body.fromId, function (err, admin) {
-            if (err || admin == null) {
-                console.log(err);
-                return;
-            }
-
-            admin.notifications.push(notification._id);
-            admin.save(function (err) {
-                if (err) {
-                    console.log(err);
-                    return;
-                }
-                Student.findById(req.body.toId, function (err, student) {
-                    if (err || student == null) {
-                        console.log(err);
-                        return;
-                    }
-
-                    student.notifications.push(notification._id);
-                    student.save(function (err) {
-                        if (err) {
-                            console.log(err);
-                            return;
-                        }
-
-                        var answer = Admin.findById(admin._id)
-                            .populate(["documents"])
-                            .populate(["notifications"])
-                            .populate(["transactions"])
-                            .then(_admin => {
-                                if (_admin == null) {
-                                    res.status(500);
-                                    res.send("User not found");
-                                } else {
-                                    console.log("Returning admin " + _admin._id);
-                                    res.json(_admin);
-                                }
-                            });
-
-                        //Done ...
-                    });
-                });
+  }
+  Admin.findById(adminID)
+    .then(user => {
+      if (user == null)
+        return res.status(514).send("User of id " + adminID + " not found");
+      var tokens = user.deviceTokens.filter(v => !v.removed).map(v => v.token);
+      if (tokens) {
+        var payload = {
+          notification: notification,
+          data: {
+            link: link,
+            props: JSON.stringify(props),
+            data: JSON.stringify(data)
+          }
+        };
+        tokens.forEach(deviceToken => {
+          FCM.sendToDevice(deviceToken, payload)
+            .then(response => {})
+            .catch(err => {
+              // return res.status(512).send(err);
             });
         });
-
-        res.json("Notification sent to " + req.body.to);
+        return res.json(
+          "Notification will be sent to " + tokens.length + " devices"
+        );
+      } else {
+        return res.status(514).send("User has no device");
+      }
+    })
+    .catch(err => {
+      return res.status(514).send(err);
     });
+});
+
+router.post(
+  "/test/push/notification/schedule/hour/:hour/minute/:minute",
+  function(req, res) {
+    var deviceToken = req.body.token;
+    var minute = req.params.minute;
+    var hour = req.params.hour;
+    var now = new Date();
+    var payload = {
+      notification: {
+        title: "Testing scheduled push!",
+        body: `This push was sent at ${hour}:${minute} , but scheduled at ${now.getHours()}:${now.getMinutes()}`
+      },
+      data: {
+        link: "/home",
+        props: JSON.stringify({
+          device_token: deviceToken
+        })
+      }
+    };
+    cronJob.schedule(`${minute} ${hour} * * *`, deviceToken, payload);
+  }
+);
+
+router.get("/cron/get/tasks", function(req, res) {
+  res.send(cronJob.getTasks());
+});
+
+router.get("/get/new/for/:userId/business/:businessID", function(req, res) {
+  var receiver = req.params.userId;
+  var businessID = req.params.businessID;
+  Notification.find({
+    businessID: businessID,
+    $or: [
+      {
+        expiryDate: null
+      },
+      {
+        expiryDate: {
+          $lt: new Date()
+        }
+      }
+    ],
+    $or: [
+      {
+        status: {
+          $ne: "SEEN"
+        }
+      }
+    ],
+    removed: false,
+    toId: receiver
+  })
+    .sort("-date")
+    .then(notifications => {
+      if (notifications == null) res.send("Error : 9032egrrtu834g9erbo");
+      res.json(notifications);
+    });
+});
+
+router.get("/all/for/:userId/:scheduled", function(req, res) {
+  var receiver = req.params.userId;
+  let scheduled = req.params.scheduled ? true : false;
+  Notification.find({
+    scheduled: scheduled,
+    $or: [
+      {
+        expiryDate: null
+      },
+      {
+        expiryDate: {
+          $lt: new Date()
+        }
+      }
+    ],
+    toId: receiver
+  }).then(notifications => {
+    if (notifications == null) res.send("Error : 9032egrrtu834g9erbo");
+    res.json(notifications);
+  });
+});
+
+router.get("/all/:userId/:scheduled", function(req, res) {
+  let sender = req.params.userId;
+  let scheduled = req.params.scheduled ? true : false;
+  Notification.find({
+    scheduled: scheduled,
+    $or: [
+      {
+        expiryDate: null
+      },
+      {
+        expiryDate: {
+          $lt: new Date()
+        }
+      }
+    ],
+    $or: [
+      {
+        fromId: sender
+      },
+      {
+        toId: sender
+      }
+    ]
+  }).then(result => {
+    if (result == null) {
+      res.status(400);
+      res.send("Error : 9032egrrtu834g9erbo");
+    }
+
+    res.json(result);
+  });
+});
+
+router.get("/tasks/all", function(req, res) {
+  Notification.find({
+    dueDate: {
+      $ne: null
+    }
+  }).then(result => {
+    if (result == null) {
+      res.status(400);
+      res.send("Error : 9032egrrtu834g9erbo");
+    }
+    res.json(result);
+  });
+});
+
+router.get("/tasks/all/:userId", function(req, res) {
+  var sender = req.params.userId;
+  Notification.find({
+    dueDate: {
+      $ne: null
+    },
+    $or: [
+      {
+        fromId: sender
+      },
+      {
+        toId: sender
+      }
+    ]
+  }).then(result => {
+    if (result == null) {
+      res.status(400);
+      res.send("Error : 9032egrrtu834g9erbo");
+    }
+    res.json(result);
+  });
+});
+
+router.post("/add", function(req, res) {
+  var notification = new Notification({
+    _id: mongoose.Types.ObjectId(),
+    fromId: req.body.fromId,
+    toId: req.body.toId,
+    title: req.body.title,
+    body: req.body.body,
+    data: req.body.data,
+    type: req.body.type,
+    status: req.body.status,
+    scheduled: req.body.scheduled,
+    topic: req.body.topic,
+    businessID: req.body.businessID,
+    scheduleInterval: req.body.scheduleInterval,
+    expiryDate: req.body.expiryDate
+  });
+
+  notification.save(function(err) {
+    if (err) return res.status(512).send(err);
+
+    Admin.findById(req.body.fromId, function(err, admin) {
+      if (err || admin == null) {
+        console.log(err);
+        return;
+      }
+
+      admin.notifications.push(notification._id);
+      admin.save(function(err) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        Student.findById(req.body.toId, function(err, student) {
+          if (err || student == null) {
+            console.log(err);
+            return;
+          }
+
+          student.notifications.push(notification._id);
+          student.save(function(err) {
+            if (err) {
+              console.log(err);
+              return;
+            }
+
+            var answer = Admin.findById(admin._id)
+              .populate(["documents"])
+              .populate(["notifications"])
+              .populate(["transactions"])
+              .then(_admin => {
+                if (_admin == null) {
+                  res.status(500);
+                  res.send("User not found");
+                } else {
+                  console.log("Returning admin " + _admin._id);
+                  res.json(_admin);
+                }
+              });
+
+            //Done ...
+          });
+        });
+      });
+    });
+
+    res.json("Notification sent to " + req.body.to);
+  });
 });
 
 module.exports = router;
