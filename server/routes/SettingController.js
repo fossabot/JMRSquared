6ab540f2a08d2712367cfbf4667e14a3eb124c8c
Sirwali Joseph <mulavhe@gmail.com>
2018-12-20 15:34:47 +0200

    Added a method to re-fire jobs and get settings

--- server/routes/SettingController.js
+++ server/routes/SettingController.js
@@ -3,14 +3,34 @@ var router = express.Router();
 import mongoose from "mongoose";
 
 import Setting from "../models/Setting";
+import CronJob from "../services/CronManager";
+const cronJob = new CronJob();
 
 router.get("/options/business/types", function (req, res) {
-  Setting.find().then(settings => {
-    if (settings == null || settings.length == 0) return res.status(512).send("No settings are avaliable.");
-    res.json(settings.pop().options["businessTypes"]);
-  }).catch(err=>{
-    return  res.status(512).send("No settings are avaliable.");
-  });
+  Setting.findOne()
+    .then(settings => {
+      if (!settings) return res.status(512).send("No settings are avaliable.");
+      res.json(settings.businessTypes);
+    })
+    .catch(err => {
+      return res.status(512).send("No settings are avaliable.");
+    });
+});
+
+router.get("/options/business/settings", function (req, res) {
+  Setting.findOne()
+    .then(settings => {
+      if (!settings) return res.status(512).send("No settings are avaliable.");
+      res.json(settings.settings);
+    })
+    .catch(err => {
+      return res.status(512).send("No settings are avaliable.");
+    });
+});
+
+router.get("/cron/reboot", function (req, res) {
+  cronJob.fireJobs();
+  res.send("Cron rebooted, firing the jobs......");
 });
 
 module.exports = router;
