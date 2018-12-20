53dd4ceba49ef8b264846653c0570c1bab2fb3dc
Sirwali Joseph <mulavhe@gmail.com>
2018-12-20 13:49:49 +0200

    Upgraded babel and made use of the new DB `JMRDevDB`

--- server/app.js
+++ server/app.js
@@ -31,7 +31,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
         "Access-Control-Allow-Headers",
@@ -53,14 +53,14 @@ mongoose.connect("mongodb://localhost:27017/JMRdb", {
 //    Server
 
 mongoose
-    .connect("mongodb://localhost:27017/JMRdb?authSource=admin", {
+    .connect("mongodb://68.66.193.181:27017/JMRDevDB?authSource=admin", {
         auth: {
             user: "admin",
             password: "Mulavhelesi@1"
         },
         reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
         reconnectInterval: 500, // Reconnect every 500ms
-        dbName: "JMRdb"
+        dbName: "JMRDevDB"
     })
     .then(answer => {
         console.log("Successfully connected to MONGO!");
@@ -77,7 +77,7 @@ app.use("/auth", authController);
 require('./config/passport');
 
 /// catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error("Not Found");
     err.status = 404;
     next(err);
@@ -88,7 +88,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get("env") === "development") {
-    app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.render("error", {
             message: err.message,
@@ -99,7 +99,7 @@ if (app.get("env") === "development") {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render("error", {
         message: err.message,
